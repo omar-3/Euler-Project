7fa92bc64484082d15202e96774d917ca59f8e12
omar-3 <s-omarkhaled@zewailcity.edu.eg>
2020-06-09 15:50:45 +0200

    more verbose

--- python/P6.py
+++ python/P6.py
@@ -1,11 +1,10 @@
 def solve(N):
 	elements = [x for x in range(1,N+1)]
-	num = len(elements)
 	i = 0
 	SUM = 0
-	while i < num - 1:
+	while i < len(elements) - 1:
 		j = i + 1
-		while j < num:
+		while j < len(elements):
 			SUM += 2 * elements[i] * elements[j]
 			j += 1
 		i += 1
